f2a9f14aa2d1bee82f5f0b870cc3d1334f990fbd
Karin99 <onestep.725a@gmail.com>
2024-09-06 16:06:58 +0900

    Apache Commons Langの追加、StringUtilの使用

--- src/main/java/raisetech/StudentManagement/Application.java
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class StudentManagementApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hola")
-	public String hola() {
-		return "Hola, World!";
-	}
+	@GetMapping("/hello")
+	public boolean hello() {
+
+		String a = "Raisetech";
+
+		return StringUtils.isNotEmpty(a);
+    }
 
 }
