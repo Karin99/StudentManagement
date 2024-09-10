ebba8b9d9c496baa19513357555357cf5df20d6e
Karin99 <onestep.725a@gmail.com>
2024-09-10 22:50:01 +0900

    GetMappingとPostMappingの追加

--- src/main/java/raisetech/StudentManagement/Application.java
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -1,22 +1,49 @@
 package raisetech.StudentManagement;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    // Map（名前、年齢）にPOSTで3人分の情報を入れて、出力する
+    // 2人目の年齢だけを入れ替える
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, world!";
+    private Map<String, String> studentMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/studentInfo")
+    public List<String> getStudentInfo() {
+        List<String> studentInfo = new ArrayList<>();
+        for (Map.Entry<String, String> entry : studentMap.entrySet()) {
+            String name = entry.getKey();
+            String age = entry.getValue();
+            studentInfo.add("名前：" + name + ",年齢：" + age);
+        }
+        return studentInfo;
+    }
+
+    @PostMapping("/studentInfo")
+    public void setStudentInfo(String name, String age) {
+        studentMap.put(name, age);
+    }
+
+    @PostMapping("/updateStudentAge")
+    public void updateStudentName(String name, String age) {
+        List<String> studentInfo = new ArrayList<>();
+        if (studentMap.containsKey(name)) {
+            studentMap.put(name, age);
+        }
+    }
 }
