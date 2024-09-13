64bc75179ddff07cb7a743d31960ad9a8d74314f
Karin99 <onestep.725a@gmail.com>
2024-09-13 11:21:59 +0900

    CRUDの実装

--- src/main/java/raisetech/StudentManagement/Application.java
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -1,49 +1,53 @@
 package raisetech.StudentManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-    // Map（名前、年齢）にPOSTで3人分の情報を入れて、出力する
-    // 2人目の年齢だけを入れ替える
-
-    private Map<String, String> studentMap = new HashMap<>();
+    @Autowired
+    private StudentRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping("/studentInfo")
-    public List<String> getStudentInfo() {
-        List<String> studentInfo = new ArrayList<>();
-        for (Map.Entry<String, String> entry : studentMap.entrySet()) {
-            String name = entry.getKey();
-            String age = entry.getValue();
-            studentInfo.add("名前：" + name + ",年齢：" + age);
+    @GetMapping("/students")
+    public String getAllStudent() {
+        String studentInfo = "";
+        List<String> studentList = new ArrayList<>();
+        for (Student student : repository.selectAllData()) {
+            studentInfo = student.getName() + " " + student.getAge() + "歳";
+            studentList.add(studentInfo);
         }
-        return studentInfo;
+        return studentList.toString();
     }
 
-    @PostMapping("/studentInfo")
-    public void setStudentInfo(String name, String age) {
-        studentMap.put(name, age);
+    @GetMapping("/student")
+    public String getStudent(@RequestParam String name) {
+        Student student = repository.searchByName(name);
+        return student.getName() + " " + student.getAge() + "歳";
     }
 
-    @PostMapping("/updateStudentAge")
-    public void updateStudentName(String name, String age) {
-        List<String> studentInfo = new ArrayList<>();
-        if (studentMap.containsKey(name)) {
-            studentMap.put(name, age);
-        }
+    @PostMapping("/student")
+    public void registerStudent(String name, int age) {
+        repository.registerStudent(name, age);
+    }
+
+    @PatchMapping("/student")
+    public void updateStudentName(String name, int age) {
+        repository.updateStudent(name, age);
+    }
+
+    @DeleteMapping("/student")
+    public void deleteStudent(String name) {
+        repository.deleteStudent(name);
     }
 }
