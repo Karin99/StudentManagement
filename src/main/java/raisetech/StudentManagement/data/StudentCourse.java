bd7f3aeaeb3f665b19767350983be1bbd28e2277
Karin99 <krnosk99@gmail.com>
2024-11-16 12:45:01 +0900

    StudentConverterTestの修正、StudentとStudentCourseのコンストラクタを追加

--- src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,8 @@ import java.time.LocalDateTime;
 /**
  * 受講生コース情報を扱うオブジェクト
  */
+@AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "受講生コース情報")
 @Getter
 @Setter
--- src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
@@ -6,6 +6,7 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,20 +17,19 @@ class StudentConverterTest {
     private StudentConverter sut;
 
     @BeforeEach
-    void before(){
+    void before() {
         sut = new StudentConverter();
     }
 
     @Test
     void 正常系_受講生リストと受講生コース情報のリストを渡したときにIDに紐づいた受講生詳細のリストが返ってくること() {
-        String id = "999";
-
-        Student student = new Student();
-        student.setId(id);
+        Student student = new Student("999", "江並浩二", "エナミコウジ", "こーじ",
+                "koji@example.com", "奈良県", 36, "男", "特になし", false);
         List<Student> studentList = List.of(student);
 
-        StudentCourse studentCourse = new StudentCourse();
-        studentCourse.setStudentId(id);
+        LocalDateTime now = LocalDateTime.now();
+        StudentCourse studentCourse = new StudentCourse(
+                "999", "999", "ピアノコース", now, now.plusYears(1));
         List<StudentCourse> studentCourseList = List.of(studentCourse);
 
         List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
@@ -40,12 +40,13 @@ class StudentConverterTest {
 
     @Test
     void 正常系_受講生リストと受講生コース情報のリストを渡したときにIDに紐づかない受講生コース情報は除外されること() {
-        Student student = new Student();
-        student.setId("999");
+        Student student = new Student("999", "江並浩二", "エナミコウジ", "こーじ",
+                "koji@example.com", "奈良県", 36, "男", "特になし", false);
         List<Student> studentList = List.of(student);
 
-        StudentCourse studentCourse = new StudentCourse();
-        studentCourse.setStudentId("111");
+        LocalDateTime now = LocalDateTime.now();
+        StudentCourse studentCourse = new StudentCourse(
+                "999", "111", "ピアノコース", now, now.plusYears(1));
         List<StudentCourse> studentCourseList = List.of(studentCourse);
 
         List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
