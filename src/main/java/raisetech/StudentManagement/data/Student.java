0e3702bf977aeddf578b19629775bdd780a63c14
Karin99 <krnosk99@gmail.com>
2024-12-26 12:06:02 +0900

    アノテーション@Dataの追加（equalsとhashcodeのオーバーライド）

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -6,12 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
  * 受講生を扱うオブジェクト
  */
+@Data
 @Schema(description = "受講生")
 @Getter
 @Setter
